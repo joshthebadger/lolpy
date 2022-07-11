20347a29ced104a0ad3d32e0d99c68b64abbe025
richard <richard.jackson@worldspan.co.uk>
2022-07-11 15:14:06 +0100

    Initial package structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lolpy
+
+This module contains routines that parse game stats rows in a structured format suitable for loading into a relational database.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='lolpy',
+    version='1.0.0',
+    description='Fetches the latest League of Legends game data.',
+    author='Josh Jackson',
+    author_email='joshisabadger@gmail.com',
+    license='',
+    packages=['lolpy'],
+    include_package_data=True,
+    install_requires=[
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Gaming/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
