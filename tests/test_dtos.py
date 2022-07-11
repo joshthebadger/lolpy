89dee671844457588040aed86435d25dc0fc24cc
richard <richard.jackson@worldspan.co.uk>
2022-07-11 15:52:58 +0100

    added test_dtos

--- tests/test_dtos.py
+++ tests/test_dtos.py
@@ -2,12 +2,19 @@ import unittest
 
 from lolpy import dtos
 
+from tests import utils
+
 
 class TestDTOs(unittest.TestCase):
 
     def test_game_serialisation(self):
         game = dtos.Game('1234')
         self.assertIsNotNone(game.gameid)
+        output_file = utils.save_item(
+            game.as_dict(),
+            'game.json'
+        )
+        print('Game saved to {}'.format(output_file))
 
 
 if __name__ == '__main__':
---
+++ tests/utils.py
@@ -0,0 +1,15 @@
+from typing import Dict
+from pathlib import Path
+
+import json
+
+TESTS_DIR = Path(__file__).parent
+FIXTURES_DIR = TESTS_DIR / 'fixtures'
+OUTPUT_DIR = TESTS_DIR / 'output'
+
+
+def save_item(item: Dict, filename: str) -> str:
+    path = Path(OUTPUT_DIR, filename)
+    with open(path, 'w') as foutput:
+        json.dump(item, foutput, indent=4)
+    return path.as_posix()
